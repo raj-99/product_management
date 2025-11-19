b5e2dd830456aebf42b2e795e2e7a3167b23b9a2
Rachit Raj <rachiraj@gmail.com>
2025-11-19 17:24:59 +0530

    updates to  Product class

--- src/Main/java/com/product_management/app/Shop.java
+++ src/Main/java/com/product_management/app/Shop.java
@@ -10,6 +10,6 @@ public class Shop {
         product1.setId(101);
         product1.setName("Laptop");
         product1.setPrice(new BigDecimal("999.99"));
-        System.out.println("Product ID: " + product1.getId() + ", Name: " + product1.getName() + ", Price: $" + product1.getPrice());
+        System.out.println("Product ID: " + product1.getId() + ", Name: " + product1.getName() + ", Price: $" + product1.getPrice() + ", Discount: $" + product1.getDiscount() + ", Discounted Price: $" + product1.getDiscountedPrice());
     }
 }
--- src/Main/java/com/product_management/data/Product.java
+++ src/Main/java/com/product_management/data/Product.java
@@ -1,29 +1,37 @@
 package com.product_management.data;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Product {
     private int id;
     private String name;
     private BigDecimal price;
-    
+    public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1); // 10% discount
+
     public int getId() {
         return id;
     }
-    public void setId(int id) {
+    public void setId(final int id) {
         this.id = id;
     }
     public String getName() {
         return name;
     }
-    public void setName(String name) {
+    public void setName(final String name) {
         this.name = name;
     }
     public BigDecimal getPrice() {
         return price;
     }
-    public void setPrice(BigDecimal price) {
+    public void setPrice(final BigDecimal price) {
         this.price = price;
+    }
+    public BigDecimal getDiscount() {
+        return price.multiply(DISCOUNT_RATE).setScale(2,RoundingMode.HALF_UP);
+    }
+    public BigDecimal getDiscountedPrice() {
+        return price.subtract(getDiscount()).setScale(2,RoundingMode.HALF_UP);
     }
 
 }
