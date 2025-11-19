fe46bf913f428af5793fe3a55ebb95eb5e249b0d
Rachit Raj <rachiraj@gmail.com>
2025-11-19 16:32:56 +0530

    added shop class

--- src/Main/java/com/product_management/app/Shop.java
+++ src/Main/java/com/product_management/app/Shop.java
@@ -1,6 +1,6 @@
-package com.product_management;
+package com.product_management.app;
 
-public class Main {
+public class Shop {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
